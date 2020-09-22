8ab849f585b8329f34c79afadb7f21ae64982c08
Ivan <djunger2008@yandex.ru>
2020-09-22 15:01:27 +0300

    make it better

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+
+bot = telebot.TeleBot('980764722:AAHN2MJcRGt8MTyezEL6L0-XvnJT8R8Qau8')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    else:
+        bot.send_message(message.from_user.id, "lolkek")
+        
+        
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
