7142ca056a72ca308ccb5bed34c244b3eeaa6f46
Ivan <djunger2008@yandex.ru>
2020-09-22 16:07:46 +0300

    Add new features

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot('980764722:AAHN2MJcRGt8MTyezEL6L0-XvnJT8R8Qau8')
 def get_text_messages(message):
     if message.text == 'Привет':
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == 'Как дела?':
+        bot.send_message(message.from_user.id, "Нормально!")
     else:
         bot.send_message(message.from_user.id, "lolkek")
         
